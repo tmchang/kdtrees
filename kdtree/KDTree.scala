785158d993387acd753c236f882bc7c14b0a0149
Lucas Adams <lucas_adams@brown.edu>
2015-03-31 23:30:21 -0400

    added method stubs

--- kdtree/KDTree.scala
+++ kdtree/KDTree.scala
@@ -1,5 +1,9 @@
 package kdtrees.kdtree
 
 class KDTree extends KDSearch[Array[Double]] {
+  
+    override def findNN(target: Array[Double]): Option[Array[Double]] = {
+      return null
+    }
 
 }
--- kdtree/Node.scala
+++ kdtree/Node.scala
@@ -1,5 +1,15 @@
 package kdtrees.kdtree
 
 class Node extends KDData[Array[Double]] {
-
+    
+    val dimensions = 0
+    
+    override def distance(other: Array[Double]): Double = {
+      0.0
+    }
+    
+    override def compDim(dim: Int, other: Array[Double]): Double = {
+      0.0
+    }
+    
 }
