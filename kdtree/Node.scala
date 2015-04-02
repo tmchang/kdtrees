297ce166d2a440e87c9c09d7c8605feb19f79443
Lucas Adams <lucas_adams@brown.edu>
2015-04-02 13:06:07 -0400

    added method stubs and node.distance

--- kdtree/Node.scala
+++ kdtree/Node.scala
@@ -1,11 +1,16 @@
 package kdtrees.kdtree
-
-class Node extends KDData[Array[Double]] {
+import scala.math
+class Node(vec: Array[Double]) extends KDData[Array[Double]] {
     
-    val dimensions = 0
+    val dimensions = vec.length
     
     override def distance(other: Array[Double]): Double = {
-      0.0
+      var toReturn: Double = 0.0
+      for (i <- 0 to dimensions){
+        toReturn += math.pow((vec(i) - other(i)),2)
+      }
+      toReturn = math.sqrt(toReturn)
+      return toReturn
     }
     
     override def compDim(dim: Int, other: Array[Double]): Double = {
